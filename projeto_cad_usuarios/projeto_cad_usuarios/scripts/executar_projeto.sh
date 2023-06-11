3554fd001d06f1a753539303ee792b39c043e798
Marcelo Medeiros dos Santos <89617160+MarceloMederi@users.noreply.github.com>
2023-06-11 16:24:40 -0300

    Update executar_projeto.sh

--- projeto_cad_usuarios/projeto_cad_usuarios/scripts/executar_projeto.sh
+++ projeto_cad_usuarios/projeto_cad_usuarios/scripts/executar_projeto.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-#!/bin/bash
-echo "Iniciando o projeto..."
-python manage.py migrate
-echo "Migrações aplicadas."
-python manage.py runserver
+# Clone o repositório do seu projeto
+git clone https://github.com/MarceloMederi/Cadastro-de-usuarios.git
 
+# Entre no diretório do projeto
+cd Cadastro-de-usuarios/projeto_cad_usuarios/projeto_cad_usuarios
+
+# Instale as dependências do projeto
+pip install -r requirements.txt
+
+# Execute as migrações do banco de dados
+python manage.py migrate
+
+# Execute o servidor Django na porta 8080
+python manage.py runserver 8080 &
+
+# Aguarde alguns segundos para o servidor iniciar
+sleep 5
+
+# Faça uma requisição HTTP para obter o HTML do projeto
+HTML=$(curl -s http://localhost:8080)
+
+# Exiba o HTML no terminal
+echo "$HTML"
+
+# Alternativamente, abra o HTML no navegador padrão
+# xdg-open http://localhost:8080
