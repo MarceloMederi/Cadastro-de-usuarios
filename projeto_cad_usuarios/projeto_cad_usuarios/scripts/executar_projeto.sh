ce556324788ded324a1352c18fd4d8449d5be3c5
Marcelo Medeiros dos Santos <89617160+MarceloMederi@users.noreply.github.com>
2023-06-12 11:02:25 -0300

    Update executar_projeto.sh

--- projeto_cad_usuarios/projeto_cad_usuarios/scripts/executar_projeto.sh
+++ projeto_cad_usuarios/projeto_cad_usuarios/scripts/executar_projeto.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
-# Clonar o repositório
-git clone https://github.com/MarceloMederi/Cadastro-de-usuarios.git
+# Comandos de instalação de dependências
+echo "Instalando as dependências..."
+pip install -r requirements.txt
 
-# Navegar para o diretório do projeto
-cd Cadastro-de-usuarios/projeto_cad_usuarios
+# Comando para obter o projeto com o curl
+echo "Obtendo o projeto..."
+curl -sSL https://raw.githubusercontent.com/MarceloMederi/Cadastro-de-usuarios/master/projeto_cad_usuarios.tar.gz -o projeto_cad_usuarios.tar.gz
 
-# Exibir a estrutura do projeto no navegador
-python -m http.server
+# Descompactar o projeto
+echo "Descompactando o projeto..."
+tar -xf projeto_cad_usuarios.tar.gz
+
+# Acessar o diretório do projeto
+cd projeto_cad_usuarios
+
+# Configurar o ambiente (se necessário)
+echo "Configurando o ambiente..."
+# Comandos de configuração do ambiente
+
+# Iniciar o servidor de desenvolvimento
+echo "Iniciando o servidor de desenvolvimento..."
+python manage.py runserver
